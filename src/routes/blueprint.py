590a70e32b4afdc6843d96199b79938afa9b63bc
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-06-25 17:14:45 -0300

    Concertando um pequeno erro, funcionando agora!

--- src/routes/blueprint.py
+++ src/routes/blueprint.py
@@ -3,6 +3,6 @@ from controllers.machineController import index, create, insert
 
 blueprint = Blueprint('blueprint', __name__)
 
-blueprint.route('/', method=['GET'])(index)
-blueprint.route('/create', method=['GET'])(create)
-blueprint.route('/insert', method=['GET'])(insert)
+blueprint.route('/', methods=['GET'])(index)
+blueprint.route('/create', methods=['GET'])(create)
+blueprint.route('/insert', methods=['GET'])(insert)
