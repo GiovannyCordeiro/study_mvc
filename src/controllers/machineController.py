3291b421ea624faf46e4cf8cb77748df40ec809f
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-06-27 17:56:38 -0300

    Refact controlers and views

--- src/controllers/machineController.py
+++ src/controllers/machineController.py
@@ -2,15 +2,16 @@ import json
 from models.machine import db
 from services.user_service import insert_logic, create_logic
 
-def index():
-    return {
+class MachineController:
+    def index():
+        return {
         'status': 'OK',
         'localhost:5000/machines/create': 'Create table in mysql database',
         'localhost:5000/machines/insert': 'Insert data in mysql database table(Inserttable)'
     }
 
-def create():
-    create_logic()
-
-def insert():
-    insert_logic()
+    def create():
+        create_logic()
+    
+    def insert():
+        insert_logic()
